3ce78c14fa03fee38f6dd33cbe2a8199d584ba27
Ithesh K <ithesh9786@gmail.com>
2026-09-21 07:28:16 +0530

    Use environment variables for database connection

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
+        host=os.environ.get("MYSQL_HOST"),
+        port=int(os.environ.get("MYSQL_PORT", 3306)),
+        user=os.environ.get("MYSQL_USER"),
         password=os.environ.get("MYSQL_PASSWORD"),
-        database="smarthealth"
+        database=os.environ.get("MYSQL_DATABASE")
     )
 @app.route("/")
 def home():
