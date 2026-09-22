e948e454648ab1049a6961d4db555b58b9d4f49f
Ithesh K <ithesh9786@gmail.com>
2026-09-22 19:13:31 +0530

    Create workout_data.py

---
+++ workout_data.py
@@ -0,0 +1,37 @@
+WORKOUT_DATA = {
+    "Sportsman": {
+        1: {
+            "focus": "Full Body",
+            "duration": 25,
+            "exercises": [
+                "Jogging - 5 min",
+                "Squats - 2 × 10",
+                "Push-ups - 2 × 8",
+                "Lunges - 2 × 8",
+                "Plank - 2 × 20 sec"
+            ]
+        },
+
+        2: {
+            "focus": "Cardio",
+            "duration": 25,
+            "exercises": [
+                "Brisk Walk - 5 min",
+                "Light Jogging - 10 min",
+                "Jumping Jacks - 2 × 10",
+                "High Knees - 2 × 15"
+            ]
+        },
+
+        3: {
+            "focus": "Lower Body",
+            "duration": 25,
+            "exercises": [
+                "Squats - 2 × 10",
+                "Lunges - 2 × 8",
+                "Glute Bridge - 2 × 10",
+                "Calf Raises - 2 × 12"
+            ]
+        }
+    }
+}
